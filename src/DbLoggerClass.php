91f3b7c016610b8e522b6def8cc66feb91bafd41
elminson <2476286+elminson@users.noreply.github.com>
2025-01-30 16:26:52 +0000

    Fix styling

--- src/DbLoggerClass.php
+++ src/DbLoggerClass.php
@@ -6,23 +6,23 @@ use Illuminate\Database\Eloquent\Builder;
 
 class DbLoggerClass
 {
-	/**
-	 * Dump the SQL query of an Eloquent builder instance.
-	 *
-	 * @param mixed $query
-	 * @return void
-	 */
-	public function ddQuery($query)
-	{
-		if ($query instanceof Builder) {
-			$sql = $query->toSql();
-			$bindings = $query->getBindings();
-			$fullSql = vsprintf(str_replace('?', '%s', $sql), array_map(function ($binding) {
-				return is_numeric($binding) ? $binding : "'{$binding}'";
-			}, $bindings));
-			dd($fullSql);
-		} else {
-			dd('The provided object is not a valid Eloquent query builder instance.');
-		}
-	}
+    /**
+     * Dump the SQL query of an Eloquent builder instance.
+     *
+     * @param  mixed  $query
+     * @return void
+     */
+    public function ddQuery($query)
+    {
+        if ($query instanceof Builder) {
+            $sql = $query->toSql();
+            $bindings = $query->getBindings();
+            $fullSql = vsprintf(str_replace('?', '%s', $sql), array_map(function ($binding) {
+                return is_numeric($binding) ? $binding : "'{$binding}'";
+            }, $bindings));
+            dd($fullSql);
+        } else {
+            dd('The provided object is not a valid Eloquent query builder instance.');
+        }
+    }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -2,16 +2,16 @@
 
 use Elminson\DbLogger\DbLoggerClass;
 
-if (!function_exists('ddQuery')) {
-	/**
-	 * Dump the SQL query of an Eloquent builder instance.
-	 *
-	 * @param mixed $query
-	 * @return void
-	 */
-	function ddQuery($query)
-	{
-		$logger = new DbLoggerClass();
-		$logger->ddQuery($query);
-	}
+if (! function_exists('ddQuery')) {
+    /**
+     * Dump the SQL query of an Eloquent builder instance.
+     *
+     * @param  mixed  $query
+     * @return void
+     */
+    function ddQuery($query)
+    {
+        $logger = new DbLoggerClass;
+        $logger->ddQuery($query);
+    }
 }
