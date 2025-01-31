0e64c2dd3773cdec5f49f3a2a95fc0d78f69eb9b
Elminson De Oleo Baez <edeoleo@gmail.com>
2025-01-30 19:41:36 -0500

    Update package name and class names to reflect new naming convention. Add new DatabaseQueryLogger class

--- src/helpers.php
+++ src/helpers.php
@@ -1,17 +1,31 @@
 <?php
 
-use Elminson\DbLogger\DbLoggerClass;
+use Elminson\DQL\DatabaseQueryLogger;
 
-if (! function_exists('ddQuery')) {
-    /**
-     * Dump the SQL query of an Eloquent builder instance.
-     *
-     * @param  mixed  $query
-     * @return void
-     */
-    function ddQuery($query)
-    {
-        $logger = new DbLoggerClass;
-        $logger->ddQuery($query);
-    }
+if (!function_exists('log_query')) {
+	/**
+	 * Dump the SQL query of an Eloquent builder instance.
+	 *
+	 * @param mixed $query
+	 * @param bool  $print
+	 * @param bool  $return
+	 *
+	 * @return string|void
+	 * @throws Exception
+	 */
+	function log_query(mixed $query, bool $print = false, bool $return = false)
+	{
+
+		$logger = new DatabaseQueryLogger();
+		$sql = $logger->logQuery($query);
+
+		if ($print) {
+			echo $sql;
+		}
+
+		if ($return) {
+			return $sql;
+		}
+
+	}
 }
